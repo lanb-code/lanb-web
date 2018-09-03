20fcefdbe625beac8f519e6c9dc7aa8bc966074f
colodoo <lodog1@qq.com>
2018-09-03 22:32:58 +0800

    增加shiro依赖 shiro登方式

--- src/main/java/com/colodoo/framework/manager/user/action/UserAction.java
+++ src/main/java/com/colodoo/framework/manager/user/action/UserAction.java
@@ -4,6 +4,9 @@ import com.colodoo.framework.manager.user.model.User;
 import com.colodoo.framework.manager.user.service.UserService;
 import com.colodoo.framework.easyui.Page;
 import com.github.pagehelper.PageInfo;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,6 +49,9 @@ public class UserAction {
         } else {
             //登录成功
             if (userService.loginCheck(model)) {
+                Subject subject = SecurityUtils.getSubject();
+                UsernamePasswordToken token = new UsernamePasswordToken(model.getUserName(), model.getPassword());
+                subject.login(token);
                 map.put("success", true);
                 map.put("msg", "登录成功!");
                 model.setPassword(null);
@@ -64,7 +70,7 @@ public class UserAction {
     public Map register(User model) {
         Map map = new HashMap();
         boolean isSuccessed = userService.register(model);
-        if(isSuccessed) {
+        if (isSuccessed) {
             map.put("success", true);
             map.put("msg", "注册成功!");
         } else {
